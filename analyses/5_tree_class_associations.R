642c0fbfcc1e8b3fd19e021b261408350c5fcf9b
CharlieWhittaker <cwhittaker1000@gmail.com>
2021-09-30 17:31:28 +0100

    getting there with understanding

--- analyses/5_tree_class_associations.R
+++ analyses/5_tree_class_associations.R
@@ -7,7 +7,7 @@ library(tidyverse); library(here); library(zoo); library(forecast); library(TSA)
 library(mgcv); library(GPfit); library(rstan); library(shinystan); library(reshape2); 
 library(deSolve); library(parallel); library(matlib); library(matlab); library(pracma); 
 library(rstan); library(ggplot2); library(invgamma); library(tictoc); library(DescTools);
-library(dismo); library(gbm); library(mltools); library(glmnet); library(caret);
+library(dismo); library(gbm); library(mltools); library(glmnet); library(caret); library(themis)
 library(tidymodels); library(doParallel); library(vip); library(forcats); library(vip)
 
 source(here("functions", "time_series_characterisation_functions.R"))
@@ -37,20 +37,27 @@ rf_train <- training(rf_split)
 rf_test <- testing(rf_split)
 
 # Prepping Data - Selecting Variables, Creating Recipe, Generating CV Folds
-envt_recipe <- recipe(peaks  ~ ., data = rf_train) %>% 
+envt_recipe <- recipe(peaks  ~ ., data = rf_train) %>%
+  #update_role(x, y, new_role = "ID") %>% # retained in the data, but not used for model fitting. 
   step_center(all_predictors(), -country) %>% 
   step_scale(all_predictors(), -country) %>%
-  step_dummy(country) 
+  step_dummy(country) #%>%
+  #step_rose(peaks) 
+summary(envt_recipe)
 envt_prepped <- prep(envt_recipe, training = rf_train, verbose = TRUE)
 juiced <- juice(envt_prepped)
 
+# dim(data)
+# dim(juiced)
+
 cv_splits <- vfold_cv(rf_train, v = 6) # v sets number of splits
 perf_metrics <- metric_set(yardstick::accuracy)
+#perf_metrics <- metric_set(yardstick::roc_auc)
 
 # Random Forest
 random_forest <- rand_forest(mtry = tune(), trees = 1000, min_n = tune()) %>%
   set_mode("classification") %>%
-  set_engine("ranger", importance = "permutation")
+  set_engine("ranger", importance = "permutation", seed = 123)
 rf_workflow <- workflow() %>%
   add_recipe(envt_recipe) %>%
   add_model(random_forest)
@@ -84,7 +91,9 @@ tune_res %>%
   labs(y = "accuracy")
 
 # Selecting the Best Model and Exploring Quality of Fit
+show_best(tune_res, "roc_auc")
 show_best(tune_res, "accuracy")
+
 best_rmse <- select_best(tune_res, "accuracy")
 
 # Extracting and plotting raw predictions
@@ -94,36 +103,89 @@ raw_pred_best_hyp <- raw_predictions %>%
   dplyr::filter(mtry == best_rmse$mtry, min_n == best_rmse$min_n)
 order_raw_pred_best_hyp <- raw_pred_best_hyp %>%
   dplyr::arrange(., .row)
+
+# checking that this gives us the OOB error model 
+# Note: this rmse calculation is NOT the same as reported in show_best exactly. But that's
+#       because this is calculating average of each of the individual predictions' errors, rather 
+#       average of the average error for each fold
 sum(order_raw_pred_best_hyp$.pred_class == order_raw_pred_best_hyp$peaks)/length(order_raw_pred_best_hyp$peaks)
-# This pretty much matches the show_best results above, so fine
+
+# calculating average error for each fold
+x <- order_raw_pred_best_hyp %>%
+  group_by(id) %>%
+  summarise(prop = sum(.pred_class == peaks)/n())
+mean(x$prop) # This matches the show_best results above, so fine
 
 # Finalising Workflow and Fitting this Model to the Entire Data 
 random_forest_final <- rf_workflow %>%
   finalize_workflow(best_rmse)
+class(random_forest_final)
 
-# checking that this gives us the OOB error model 
-# this rmse calculation is NOT the same as reported in show_best exactly. But that's
-# because this is calculating average of each of the individual predictions' errors, rather 
-# average of the average error for each fold
+# note when I use ROSE oversampling, this code doesn't run
+# *because* x$fit$fit$fit$predictions has predictions from initial training,
+# not from a new fit to the whole rf_train as far as I can make out. 
+# Nah don't think so, think it's refitting to rf_train, but probably 
+# running rf_train through the recipe
+temp <- rf_train#[-c(1:10), ]
 x <- random_forest_final %>%
-  fit(data = rf_train)
+  fit(data = temp)
+dim(x$fit$fit$fit$predictions)[1] == dim(temp)[1]
+extract_fit_parsnip(x) # but this says sample size is 52 which size of rf_train[-1, ]
+x$fit$fit$fit$predictions
 pred <- ifelse(x$fit$fit$fit$predictions[, 1] > 0.50, 1, 2)
-sum(pred == order_raw_pred_best_hyp$peaks)/length(order_raw_pred_best_hyp$peaks)
+sum(pred == temp$peaks)/length(temp$peaks)
+# so think we're relatively clear now that the above should be fitting the whole rf_train 
+# to the optimised random forest, tuned hyperparameter workflow. 
+# fit is NOT significant better than OOS predictions, but is a bit better (0.77 vs 0.79)
 
-# calculating variable importance
-set.seed(1)
+# fit IS better when predicting and using whole forest of trees (as expected)
+# though you can see it's done v good at predicting 1 peak, basically no better than random at predicting 
+# 2 peaks
+predict(x, rf_train)
+sum(as.numeric(rf_train$peaks) == predict(x, rf_train)$.pred_class)/length(predict(x, rf_train)$.pred_class)
+table(as.numeric(rf_train$peaks), as.numeric(predict(x, rf_train)$.pred_class))
+
+# fit is EVEN better when predicting on test set
+# confusing but I think due to class imbalance and tendency to just predict 1s
+# yeah same issue here, mainly predicts 1s, because dataset is mainly 1s, does well
+predict(x, rf_test)
+sum(as.numeric(rf_test$peaks) == predict(x, rf_test)$.pred_class)/length(predict(x, rf_test)$.pred_class)
+table(as.numeric(rf_test$peaks), as.numeric(predict(x, rf_test)$.pred_class))
+
+# calculating variable importance #1
 random_forest_final %>%
   fit(data = rf_train) %>%
-  pull_workflow_fit() %>%
+  extract_fit_parsnip() %>%
   vip(geom = "point")
 
-# alternative way of calculating variable importance (confirm identical to above with same seed)
+# alternative way of calculating variable importance #2
+# THIS IS A MODEL NOT A WORKFLOW, THEREFORE DOESN'T HAVE THE 
+# RECIPE ASSOCIATED WITH IT; THEREFORE WILL NOT PROCESS
+# THE COUNTRY COLUMN, THEREFORE DIFFERNET RESULTS
 final_rf <- finalize_model(random_forest, best_rmse)
-set.seed(1)
+class(final_rf)
 final_rf %>%
-  set_engine("ranger", importance = "permutation") %>%
+  set_engine("ranger", importance = "permutation", seed = 123) %>%
   fit(peaks  ~ ., data = rf_train) %>%
   vip(geom = "point")
+
+a <- random_forest_final %>% # 1 "A workflow" - does the prepping step and one hot encoding etc
+  fit(data = rf_train) %>%
+  extract_fit_parsnip()
+
+b <- final_rf %>% # 2 "A Model" - does not do the prepping step of one hot encoding etc
+  set_engine("ranger", importance = "permutation", seed = 123) %>%
+  fit(peaks  ~ ., data = rf_train) 
+
+a$fit$predictions[1:3, ]
+b$fit$predictions[1:3, ]
+
+# CURRENTLY THESE TWO ARE DIFFERENT TO EACH OTHER - because categorical is rolled into one
+# factor for B (instead of one hot encoded), which I think probably explains why (maybe)
+a$fit$forest$independent.variable.names
+b$fit$forest$independent.variable.names
+length(a$fit$variable.importance)
+length(b$fit$variable.importance)
 
 unregister_dopar <- function() {
   env <- foreach:::.foreachGlobals
@@ -134,20 +196,52 @@ unregister_dopar()
 final_res <- random_forest_final %>%
   last_fit(rf_split) 
 
-# final_res <- random_forest_final %>%
-#   fit(data) %>%
-#   pull_workflow_fit() %>%
-#   vip(geom = "point")
-
-set.seed(1)
+# Should be same as variable importance calculation #1 
 extract_workflow(final_res) %>%
   extract_fit_parsnip() %>%
   vip(geom = "point")
 
+# extracts the fitted model and predictions from the workflow
+# still OOS random forest predictions though
+y <- extract_workflow(final_res)
+y$fit$fit$fit$predictions
+pred <- ifelse(y$fit$fit$fit$predictions[, 1] > 0.50, 1, 2)
+sum(pred == temp$peaks)/length(temp$peaks)
+# matches the fitting and OOS, pruning based prediction on lines 129 - 136 (approx)
+
+z <- predict(y, rf_train)
+sum(pred == z$.pred_class)/length(z$.pred_class)
+# why is this different to 
+#     predict(x, rf_train)
+#     sum(as.numeric(rf_train$peaks) == predict(x, rf_train)$.pred_class)/length(predict(x, rf_train)$.pred_class)
+# on ~ lines 144 and 145 above. 
+# both are predicting on the full random_forest
+
+d <- predict(y, rf_test)
+sum(as.numeric(rf_test$peaks) == d$.pred_class)/length(d$.pred_class)
+# matches the above previous predict call on rf_test
+# doubly weird given the above one doesn't match
+
+# these are far higher than 
 final_res %>%
   collect_metrics()
 
 sum(final_res$.predictions[[1]]$peaks == final_res$.predictions[[1]]$.pred_class)/length(final_res$.predictions[[1]]$.pred_class)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 ####################################################
